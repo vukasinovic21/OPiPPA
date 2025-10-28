010be7ba0d4be3f74b48e9db63590981c350e7eb
Nikola Vukasinovic <70863561+vukasinovic21@users.noreply.github.com>
2025-10-28 11:27:56 +0100

    Add files via upload

---
+++ pregledi2025/src/main/java/fin/kg/ac/rs/pregledi2025/Pregledi2025Application.java
@@ -0,0 +1,13 @@
+package fin.kg.ac.rs.pregledi2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pregledi2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pregledi2025Application.class, args);
+	}
+
+}
---
+++ pregledi2025/src/test/java/fin/kg/ac/rs/pregledi2025/Pregledi2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package fin.kg.ac.rs.pregledi2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pregledi2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
